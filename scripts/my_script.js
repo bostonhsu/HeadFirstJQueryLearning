923d1aad6997248757fe56a70f5ad3f368b44224
徐智勇 <bostonhsu@outlook.com>
2018-12-07 11:10:43 +0800

    Step2-9

---
+++ scripts/my_script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    const checkForCode = function () {
+        var discount = Math.floor((Math.random() * 5) + 5);
+        var discount_msg = "<p>Your Discount is " + discount + "%</p>";
+        $(this).append(discount_msg);
+        $(".guess_box").unbind("click");
+    };
+    $(".guess_box").click(checkForCode);
+});
