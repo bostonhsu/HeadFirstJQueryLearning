2859dd8cf99dc20e68ed7bcc5049cbc020be3030
徐智勇 <bostonhsu@outlook.com>
2018-12-07 23:25:09 +0800

    Step3-1

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -1,18 +1,19 @@
 $(document).ready(function() {
-    const checkForCode = function () {
-        var discount;
-        if ($.contains(this, document.getElementById("has_discount"))) {
-            var my_num = getRandom(5);
-            discount = "<p>Your Discount is " + my_num + "%</p>";
-        } else {
-            discount = "<p>Sorry, no discount this time!</p>";
-        }
-        $(this).append(discount);
-        $(".guess_box").unbind("click");
-    };
     $(".guess_box").click(checkForCode);
     hideCode();
 });
+
+function checkForCode() {
+    var discount;
+    if ($.contains(this, document.getElementById("has_discount"))) {
+        var my_num = getRandom(5);
+        discount = "<p>Your Discount is " + my_num + "%</p>";
+    } else {
+        discount = "<p>Sorry, no discount this time!</p>";
+    }
+    $(this).append(discount);
+    $(".guess_box").unbind("click");
+};
 
 function getRandom(num) {
     return Math.floor(Math.random() * num);
