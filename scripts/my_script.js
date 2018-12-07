cb262e425fcc552e5df920ed1471b211cc3f887e
徐智勇 <bostonhsu@outlook.com>
2018-12-07 11:33:13 +0800

    Step2-11

--- scripts/my_script.js
+++ scripts/my_script.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
     const checkForCode = function () {
-        var discount = Math.floor((Math.random() * 5) + 5);
-        var discount_msg = "<p>Your Discount is " + discount + "%</p>";
-        $(this).append(discount_msg);
+        var discount;
+        if ($.contains(this, document.getElementById("has_discount"))) {
+            var my_num = getRandom(5);
+            discount = "<p>Your Discount is " + my_num + "%</p>";
+        } else {
+            discount = "<p>Sorry, no discount this time!</p>";
+        }
+        $(this).append(discount);
         $(".guess_box").unbind("click");
     };
     $(".guess_box").click(checkForCode);
+    hideCode();
 });
 
 function getRandom(num) {
     return Math.floor(Math.random() * num);
-}
+};
+
+var hideCode = function() {
+    var numRand = getRandom(4);
+    $(".guess_box").each(function(index, value) {
+        if (numRand == index) {
+            $(this).append("<span id='has_discount'></span>");
+            return false;
+        }
+    });
+};
