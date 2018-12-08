d99b08f891a55471df149dd48d5db9e56db8f437
徐智勇 <bostonhsu@outlook.com>
2018-12-08 15:16:55 +0800

    Step5-done.

--- scripts/my_scripts5.js
+++ scripts/my_scripts5.js
@@ -1,12 +1,59 @@
 $(document).ready(function(){
+    var headclix = 0, eyeclix = 0, noseclix = 0, mouthclix = 0; 
     lightning_noe();
     lightning_two();
     lightning_three();
 
-    $("#head").click(clickHandler);
-    $("#eyes").click(clickHandler);
-    $("#nose").click(clickHandler);
-    $("#mouth").click(clickHandler);
+    $("#head").click(function () {
+
+		if (headclix < 9) {
+			$("#head").animate({ left: "-=367px" }, 500);
+			headclix += 1;
+		}
+		else {
+			$("#head").animate({ left: "0px" }, 500);
+			headclix = 0;
+		}
+
+	});
+
+
+	$("#eyes").click(function () {
+
+		if (eyeclix < 9) {
+			$("#eyes").animate({ left: "-=367px" }, 500);
+			eyeclix += 1;
+		}
+		else {
+			$("#eyes").animate({ left: "0px" }, 500);
+			eyeclix = 0;
+		}
+	});
+
+
+	$("#nose").click(function () {
+		if (noseclix < 9) {
+			$("#nose").animate({ left: "-=367px" }, 500);
+			noseclix += 1;
+		}
+		else {
+			$("#nose").animate({ left: "0px" }, 500);
+			noseclix = 0;
+		}
+	});//end click
+
+	$("#mouth").click(function () {
+
+		if (mouthclix < 9) {
+			$("#mouth").animate({ left: "-=367px" }, 500);
+			mouthclix += 1;
+		}
+		else {
+			$("#mouth").animate({ left: "0px" }, 500);
+			mouthclix = 0;
+		}
+
+	});
 });//end doc.onready function
 
 function clickHandler() {
