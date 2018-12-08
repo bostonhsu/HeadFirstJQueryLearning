b664327fcf27a102cd0cb17083338b6e3ef9541f
徐智勇 <bostonhsu@outlook.com>
2018-12-08 09:13:00 +0800

    Step5-5

--- scripts/my_scripts5.js
+++ scripts/my_scripts5.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
-	
-
+    $("#head").click(clickHandler);
+    $("#eyes").click(clickHandler);
+    $("#nose").click(clickHandler);
+    $("#mouth").click(clickHandler);
 });//end doc.onready function
 
+function clickHandler() {
+    var clix = 0;
+    if (clix < 9) {
+        clix += 1;
+    } else {
+        clix = 0;
+    }
+}
