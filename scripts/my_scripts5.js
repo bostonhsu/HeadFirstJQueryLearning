4ded38beb9728e12f9d3ee4cdcf0136a0ad27e83
徐智勇 <bostonhsu@outlook.com>
2018-12-08 12:46:00 +0800

    Step5-6

--- scripts/my_scripts5.js
+++ scripts/my_scripts5.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+    lightning_noe();
+    lightning_two();
+    lightning_three();
+
     $("#head").click(clickHandler);
     $("#eyes").click(clickHandler);
     $("#nose").click(clickHandler);
@@ -13,3 +17,18 @@ function clickHandler() {
         clix = 0;
     }
 }
+
+function lightning_noe(t) {
+    $("#container #lightning1").fadeIn(250).fadeOut(250);
+    setTimeout("lightning_one()", 4000);
+};
+
+function lightning_two(t) {
+    $("#container #lightning2").fadeIn("fast").fadeOut("fast");
+    setTimeout("lightning_two()", 5000);
+};
+
+function lightning_three(t) {
+    $("#container #lightning3").fadeIn("fast").fadeOut("fast");
+    setTimeout("lightning_three()", 7000);
+};
