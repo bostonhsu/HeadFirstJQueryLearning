e20ffcc9ea0793b6373ec17af3deb1c95f2d3c2e
徐智勇 <bostonhsu@outlook.com>
2018-12-10 13:18:03 +0800

    Step9-5

--- service.php
+++ service.php
@@ -1,12 +1,25 @@
 <?php
 	$query = "SELECT first_name, last_name, gender, finish_time from runners order by finish_time ASC ";
 	$result = db_connection($query);
+	$runners = array();
+
 	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-		print_r($row);
+		array_push($runners, array('fname' => $row['first_name'], 'lname' => $row['last_name'], 'gender' => $row['gender'], 'time' => $row['finish_time']));
 	}
+	echo json_decode(array("runners" => $runners));
+	exit;
+
 	function db_connection($query) {
 		mysql_connect('localhost', 'runner_db_user', 'runner_db_password') OR die ('Could not connect to database.');
 		mysql_select_db('hfjq_race_info');
 		return mysql_query($query);
+	}
+
+	function fail($message) {
+		die(json_encode(array('status' => 'fail', 'message' => $message)));
+	}
+
+	function success($message) {
+		die(json_encode(array('status' => 'success', 'message' => $message)));
 	}
 ?>
