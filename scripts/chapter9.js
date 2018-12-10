668c27de4fa4f11fbb1415fc755f6eb9ce4b07a8
徐智勇 <bostonhsu@outlook.com>
2018-12-10 13:13:21 +0800

    Step9-4

--- scripts/chapter9.js
+++ scripts/chapter9.js
@@ -1,82 +1,83 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-	var FREQ = 10000 ;
+	var FREQ = 10000;
 	var repeat = true;
-	
-	function showFrequency(){
-		$("#freq").html( "Page refreshes every " + FREQ/1000 + " second(s).");
+
+	function showFrequency() {
+		$("#freq").html("Page refreshes every " + FREQ / 1000 + " second(s).");
 	}
-	
-	function startAJAXcalls(){
-	
-		if(repeat){
-			setTimeout( function() {
-					getXMLRacers();
-					startAJAXcalls();
-				}, 	
+
+	function startAJAXcalls() {
+
+		if (repeat) {
+			setTimeout(function () {
+				getDBRacers();
+				getXMLRacers();
+				startAJAXcalls();
+			},
 				FREQ
 			);
 		}
 	}
 
 	function getDBRacers() {
-		$.getJSON("service.php", function(json) {
+		$.getJSON("service.php", function (json) {
 			alert(json.runners.length);
 		});
 		getTimeAjax();
 	}
-	
-	function getXMLRacers(){
+
+	function getXMLRacers() {
 		$.ajax({
 			url: "finishers.xml",
 			cache: false,
 			dataType: "xml",
-			success:  function(xml){
-				
+			success: function (xml) {
+
 				$('#finishers_m').empty();
 				$('#finishers_f').empty();
 				$('#finishers_all').empty();
-				
-				$(xml).find("runner").each(function() {
+
+				$(xml).find("runner").each(function () {
 					var info = '<li>Name: ' + $(this).find("fname").text() + ' ' + $(this).find("lname").text() + '. Time: ' + $(this).find("time").text() + '</li>';
-					if( $(this).find("gender").text() == "m" ){
-						$('#finishers_m').append( info );
-					}else if ( $(this).find("gender").text() == "f" ){
-						$('#finishers_f').append( info );
-					}else{  }
-					$('#finishers_all').append( info );
+					if ($(this).find("gender").text() == "m") {
+						$('#finishers_m').append(info);
+					} else if ($(this).find("gender").text() == "f") {
+						$('#finishers_f').append(info);
+					} else { }
+					$('#finishers_all').append(info);
 				});
-				
+
 				getTimeAjax();
 			}
 		});
 	}
 
-	function getTimeAjax(){
+	function getTimeAjax() {
 		var time = "";
 		$.ajax({
 			url: "time.php",
 			cache: false,
-			success: function(data){
+			success: function (data) {
 				$('#updatedTime').html(data);
 			}
 		});
 	}
-	
-	$("#btnStop").click(function(){
+
+	$("#btnStop").click(function () {
 		repeat = false;
-		$("#freq").html( "Updates paused." );
+		$("#freq").html("Updates paused.");
 	});
 
-	$("#btnStart").click(function(){
+	$("#btnStart").click(function () {
 		repeat = true;
 		startAJAXcalls();
 		showFrequency();
-	});	
+	});
 
-	$("#btnSave").click(function() {
+	$("#btnSave").click(function () {
 		var data = $("#addRunner : input").serializeArray();
-		$.post($("#addRunner").attr('action'), data, function(json) {
+		$.post($("#addRunner").attr('action'), data, function (json) {
 			if (json.status == "fail") {
 				alert(json.message);
 			}
@@ -88,12 +89,12 @@ $(document).ready(function(){
 	});
 
 	function clearInputs() {
-		$("#addRunner :input").each(function() {
+		$("#addRunner :input").each(function () {
 			$(this).val('');
 		});
 	}
 
-	$("#addRunner").submit(function() {
+	$("#addRunner").submit(function () {
 		return false;
 	});
 
